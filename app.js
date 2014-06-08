ed14c8e1ff8cd3e7f93e3e025f47a1a81df41c1c
Andy Cheung <skyleft123@gmail.com>
2014-06-08 10:25:12 +0800

    美化、完善、完成基本功能

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ var server = ss.simpleServer;
 server.listen(8888);
 
 si.listen(server).on('connection', function (socket) {
+    var address = socket.handshake.address.address;
     socket.on('message', function (msg) {
-        console.log('Message Received: ', msg);
-        socket.broadcast.emit('message', msg);
+        console.log('message in from: ', address,msg);
+        socket.broadcast.emit('message', '['+address+']说: '+msg);
     });
 });
